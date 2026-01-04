09201f558c3fcfcfc0b7e932afce236d50bae38b
seungwoo1209 <2002msw@naver.com>
2026-01-05 05:07:02 +0900

    updated bundle

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1724552134',resources: [
+    cacheName: 'chirpy-1767557154',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
@@ -15,16 +15,22 @@ const swconf = {
 
       
       
-        '/assets/js/dist/app.min.js',
         '/assets/js/dist/categories.min.js',
         '/assets/js/dist/commons.min.js',
         '/assets/js/dist/home.min.js',
         '/assets/js/dist/misc.min.js',
         '/assets/js/dist/page.min.js',
-        '/assets/js/dist/post.min.js'
-    ],denyPaths: [
-      
+        '/assets/js/dist/post.min.js',
+        '/assets/js/dist/theme.min.js'
     ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
     purge: false
   
 };
